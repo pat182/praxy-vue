455b10c917cea45a726eb9a2feabae34d1ff7f43
unknown <patathome182@gmail.com>
2023-06-25 19:50:40 +0800

    product page wip

--- src/components/section-header-comp.js
+++ src/components/section-header-comp.js
@@ -1,6 +1,6 @@
 export const sec_head_comp = {
 	props : ['title'],
-	template : `<div class='content-header'>
+	template : `<div class='main-content-header'>
 				    <h3>{{title}}</h3>  
 				</div>`
 }
